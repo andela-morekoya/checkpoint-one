f6bb484be2a2e137b561c458f086a38186802bc8
andela-morekoya <morolake.orekoya@andela.com>
2016-08-25 10:22:15 +0100

    fix range definition

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,76 @@
+require_relative "message"
+
+class MusicLibraryController
+  include
+
+  def initialize(path = "./db/mp3s")
+    MusicImporter.new(path).import
+  end
+
+  def call
+    Message.intro
+    command = gets.chomp.downcase
+    run_command(command)
+  end
+
+  def run_command(command)
+    if command != "exit"
+      begin
+        send(command.tr(" ", "_"))
+      rescue
+        Message.invalid
+      end
+      call
+    end
+  end
+
+  def list_songs
+    Song.all.each.with_index(1) do |song, index|
+      Message.song_name_with_index(index, song)
+    end      
+  end
+
+  def list_artists
+    Artist.all.each {|item| puts item.name}
+  end
+
+  def list_genres
+    Genre.all.each {|item| puts item.name}
+  end
+
+  def play_song
+    size = Song.all.length
+    Message.what_song(size)
+    selection = gets.chomp.to_i - 1
+
+    if (0..size).include?(selection)
+      Message.playing(Song.all[selection])
+    else 
+      Message.invalid_song
+    end
+  end
+
+  def list_artist
+    Message.get_name(Artist)
+    result = Artist.find_by_name(gets.chomp)
+    print_data(result)
+  end
+
+  def list_genre
+    Message.get_name(Genre)
+    result = Genre.find_by_name(gets.chomp)
+    print_data(result)
+  end
+
+  def print_data(klass)
+    if klass
+      klass.songs.each {|song| Message.song_name(song)}
+    else
+      Message.not_found(klass)
+    end
+  end
+
+  def help
+    Message.list_commands
+  end
+end
